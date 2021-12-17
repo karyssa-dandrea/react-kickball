60a27b7fcaa08d84811a83b79380cbf97a4f5bc0
karyssa-dandrea <karyssadandrea@gmail.com>
2021-12-16 16:26:09 -0800

    trying to add my routes and idk why it looks weird

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <BrowserRouter>
+  <header>
+    <NavLink to "/teams"> Teams </NavLink>
+      <NavLink to "/players"> Players </NavLink>
+    </header>
+    <Switch>
+      <Route Path="/teams/:id" component={TeamDetail} />
+      <Route Path="/teams" component={TeamList} />
+      <Route Path="/players/:id" component={PlayerDetail} />
+      <Route Path="/players" component={PlayerList} />
+    </Switch>
+    </BrowserRouter>
+    );
 }
 
 export default App;
