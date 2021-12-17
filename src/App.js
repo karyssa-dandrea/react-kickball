1660e9c78d8d84a3a3a16ec0ec6d6687a3cda4a4
karyssa-dandrea <karyssadandrea@gmail.com>
2021-12-16 16:31:48 -0800

    needed = signs and imported everything i needed for app.js

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
 import './App.css';
 import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom';
-
+import TeamList from './views/Teams/TeamList';
+import TeamDetail from './views/Teams/TeamDetail';
+import PlayerDetail from './views/Players/PlayerDetail';
+import PlayerList from './views/Players/PlayerList';
 
 function App() {
   return (
-  <BrowserRouter>
-  <header>
-    <NavLink to "/teams"> Teams </NavLink>
-      <NavLink to "/players"> Players </NavLink>
-    </header>
-    <Switch>
-      <Route Path="/teams/:id" component={TeamDetail} />
-      <Route Path="/teams" component={TeamList} />
-      <Route Path="/players/:id" component={PlayerDetail} />
-      <Route Path="/players" component={PlayerList} />
-    </Switch>
+    <BrowserRouter>
+      <header>
+        <NavLink to="/teams"> Teams </NavLink>
+        <NavLink to="/players"> Players </NavLink>
+      </header>
+      <Switch>
+        <Route Path="/teams/:id" component={TeamDetail} />
+        <Route Path="/teams" component={TeamList} />
+        <Route Path="/players/:id" component={PlayerDetail} />
+        <Route Path="/players" component={PlayerList} />
+      </Switch>
     </BrowserRouter>
-    );
+  );
 }
 
 export default App;
