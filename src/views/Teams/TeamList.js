8763befc3e2f478aaa17a5dce1ed0ef52883d101
karyssa-dandrea <karyssadandrea@gmail.com>
2021-12-16 17:07:42 -0800

    added my team.js files, had help from ach

--- src/views/Teams/TeamList.js
+++ src/views/Teams/TeamList.js
@@ -1,5 +1,10 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 
-export default function TeamList() {
+export default function TeamList(props) {
+  const id = props.match.params.id;
+  const [team, setTeams] = useState(null);
+
+  useEffect(() => {});
   return <div></div>;
 }
