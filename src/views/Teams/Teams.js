5f40b026944f373c89ba303c543f7e770189fa10
karyssa-dandrea <karyssadandrea@gmail.com>
2021-12-17 12:28:56 -0800

    added y useefeect and map in team.js, not sure if this is the right spot for it

--- src/views/Teams/Teams.js
+++ src/views/Teams/Teams.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
+import { fetchTeams } from '../../services/teams';
+import { Link } from 'react-router-dom';
 
-export default function TeamList() {
-  return <div></div>;
+export default function Teams() {
+  const [teams, setTeams] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      const data = await fetchTeams();
+      setTeams(data);
+    };
+    fetchData();
+  }, []);
+  return (
+    <div>
+      <h1>Teams</h1>
+      {teams.map((team) => (
+        <Link key={team.id} to={`/teams/${team.id}`}>
+          {team.name}
+        </Link>
+      ))}
+    </div>
+  );
 }
