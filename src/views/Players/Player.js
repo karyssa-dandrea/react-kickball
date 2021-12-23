0d374750718f23225dc14d7dbf90b3a05f87e894
karyssa-dandrea <karyssadandrea@gmail.com>
2021-12-22 17:39:33 -0800

    passed an id into player.js and used params.match

--- src/views/Players/Player.js
+++ src/views/Players/Player.js
@@ -2,15 +2,17 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 import { fetchPlayers } from '../../services/players';
 import PlayerList from './Players';
-export default function Player() {
+export default function Player(props) {
   const [players, setPlayers] = useState([]);
+  const id = props.match.params.id;
+
   useEffect(() => {
     const fetchData = async () => {
-      const data = await fetchPlayers();
+      const data = await fetchPlayers(id);
       setPlayers(data);
     };
     fetchData();
-  }, []);
+  }, [id]);
   return (
     <div>
       <h1>Players</h1>
