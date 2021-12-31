d6f1ad8f7d253276275f35865d98fc7e8f0cb221
karyssa-dandrea <karyssadandrea@gmail.com>
2021-12-30 17:23:25 -0800

    cant figure out why my players isnt working

--- src/components/PlayerFunction/PlayerList.js
+++ src/components/PlayerFunction/PlayerList.js
@@ -3,17 +3,15 @@ import { Link } from 'react-router-dom';
 
 export default function PlayerList({ players }) {
   return (
-    <div className="players-list">
+    <>
       <h1>List of Players</h1>
-      <ul>
+      <div className="players-list">
         {players.map((players) => (
-          <li key={players.id}>
-            <Link key={players.id} to={`/players/${players.id}`}>
-              {players.name}
-            </Link>
-          </li>
+          <Link key={players.id} to={`/players/${players.id}`}>
+            {players.name}
+          </Link>
         ))}
-      </ul>
-    </div>
+      </div>
+    </>
   );
 }
