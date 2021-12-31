31360729a322a181dd41bb5c7a259bfdcab6ac65
karyssa-dandrea <karyssadandrea@gmail.com>
2021-12-30 17:11:34 -0800

    added my loading state to player

--- src/components/PlayerFunction/PlayerDetail.js
+++ src/components/PlayerFunction/PlayerDetail.js
@@ -4,6 +4,7 @@ export default function PlayerDetail({ player }) {
   return (
     <div>
       <h3>{player.name}</h3>
+      <p>Position: {player.position}</p>
     </div>
   );
 }
--- src/views/Players/Player.js
+++ src/views/Players/Player.js
@@ -4,20 +4,27 @@ import { fetchPlayerById } from '../../services/players';
 import PlayerDetail from './Players';
 export default function Player(props) {
   const [player, setPlayer] = useState([]);
+  const [loading, setLoading] = useState(true);
   const id = props.match.params.id;
 
   useEffect(() => {
     const fetchData = async () => {
       const data = await fetchPlayerById(id);
       setPlayer(data);
+      setLoading(false);
     };
     fetchData();
   }, [id]);
+
+  if (loading) {
+    return <h1>Loading</h1>;
+  }
+
   return (
     <div className="player-deets">
       <h1>Players</h1>
       <p>{player.name}</p>
-      <PlayerDetail players={player} />
+      <PlayerDetail player={player} />
     </div>
   );
 }
